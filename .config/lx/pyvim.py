a7c61c55e8b56ee55a44cc7df2382c49cf97e6ef
lxhillwind <lxhillwind@gmail.com>
2022-03-27 14:48:38 +0800

    add some local config

---
+++ .config/lx/pyvim.py
@@ -0,0 +1,112 @@
+# vim:ft=python
+# path: ~/vimfiles/config/pyvim.py
+
+import os
+import pathlib
+import textwrap
+import asyncio
+import typing
+import shlex
+from pyvim.libvim import Worker as BaseWorker, vim
+
+# this file may be loaded with exec(), so __file__ can't be used to get cwd.
+# so use python process startup cwd (set in plugin/server.vim) and join pyvim.
+CWD = pathlib.Path().cwd().joinpath('pyvim')
+
+PYVIM_RC = os.getenv('PYVIM_RC')
+
+
+class Worker(BaseWorker):
+
+    # it's recommended to keep the config method defined below in "worker.py",
+    # then you can open it easily.
+    #
+    # you can define more methods in this class, and they will appear once
+    # the python process is reloaded (restart vim or `:Py3 restart`).
+
+    async def config(self, _args: str, bang: bool = False):
+        """open worker.py (pyvim user config file)"""
+        config_file = CWD.joinpath('worker.py')
+        if PYVIM_RC:
+            config_file = pathlib.Path(PYVIM_RC)
+        f_exists = config_file.exists()
+        f_escaped = await vim.fn.fnameescape(str(config_file))
+        f_editcmd = 'e' if bang else 'tabe'
+        await vim.cmd(f_editcmd + ' ' + f_escaped)
+        if not f_exists:
+            content = textwrap.dedent('''\
+                # This file is generated from "worker.py.example";
+                # once you save this file ("worker.py") to disk,
+                # `:Py3` command will load worker.py instead of worker.py.example.
+            ''')
+            content += '\n'
+            with CWD.joinpath('worker.py.example').open() as f:
+                content += f.read()
+            await vim.fn.append('$', content.rstrip('\n').split('\n'))
+
+    async def eval(self, args: str, range: int = 0, line1: int = 0, line2: int = 0):
+        """eval range selection or string after `:Py3 eval`."""
+        if range in (1, 2):
+            args = '\n'.join(await vim.fn.getline(line1, line2))
+        func = 'async def _local():\n' + textwrap.indent(args, ' ' * 4)
+        exec(func)
+        await locals()['_local']()
+
+    async def dirlist(self, args, **kwargs):
+        if await vim.eval('&ft') != 'dirlist':
+            await vim.cmd('setl buftype=nofile')
+            await vim.cmd('setl ft=dirlist')
+            await vim.cmd('nnoremap <buffer> - <cmd>Py3 dirlist %:h<CR>')
+            await vim.cmd('nnoremap <buffer> i <cmd>Py3 dirlist <line><CR>')
+        if len(args) > 0:
+            dirname = args
+        else:
+            dirname = '%'
+
+        if dirname == '<line>':
+            path = await vim.fn.fnamemodify(await vim.fn.getline('.'), ':p')
+        else:
+            path = await vim.fn.fnamemodify(await vim.fn.expand(dirname), ':p')
+
+        import pathlib
+        path = pathlib.Path(path)
+        if not path.exists():
+            await vim.cmd('echohl WarningMsg | echomsg "file not exists!" | echohl None')
+            return
+        if not path.is_dir():
+            await vim.cmd(f'e %s' % await vim.fn.fnameescape(str(path)))
+            return
+        result = [
+                f'{i}/' if i.is_dir() else f'{i}'
+                for i in path.iterdir()
+                ]
+        await vim.cmd('noswapfile file %s' % await vim.fn.fnameescape(str(path)))
+        await vim.fn.deletebufline('', 1, '$')
+        await vim.fn.append(0, result)
+        await vim.fn.deletebufline('', '$')
+
+    async def term(self, args):
+        buf = await vim.fn.term_start(shlex.split(args) if args else ['/bin/zsh'], {
+            'hidden': 1, 'term_finish': 'close'
+            })
+        await vim.fn.popup_create(buf, {
+            'minwidth': 80, 'minheight': 24,
+            'maxwidth': 80, 'maxheight': 24,
+            'border': [],
+            })
+
+
+async def test_performance(self: Worker, arg):
+    import datetime
+    now = datetime.datetime.now()
+    import json
+    arg = int(arg)
+    print(len(json.dumps(await vim.fn.range(arg), ensure_ascii=False)))
+    await vim.cmd('let g:MyFoo = 0')
+    for i in await vim.fn.range(arg):
+        await vim.cmd('let g:MyFoo += %s' % i)
+    now_1 = datetime.datetime.now()
+    print(now, await vim.eval('g:MyFoo'), now_1 - now)
+
+
+Worker.test_performance = test_performance
