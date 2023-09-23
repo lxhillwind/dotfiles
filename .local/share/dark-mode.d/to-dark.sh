6f4ce6fc44b494fa692466c29da4ccec69f0f92d
lxhillwind <lxhillwind@foxmail.com>
2023-09-23 09:57:15 +0800

    darkman config: xfce4-terminal switched to xfconf

--- .local/share/dark-mode.d/to-dark.sh
+++ .local/share/dark-mode.d/to-dark.sh
@@ -33,15 +33,29 @@ else
 fi
 # }}}
 
-# xfce4-terminal {{{
-# config path:
-# ~/.config/xfce4/terminal/terminalrc
+# xfce4-terminal (>=1.1.0), using xfconf {{{
 #
 # How to get color config for light / dark theme?
 # Switch theme manually, and inspect config.
+if [ -n "$(xfconf-query -c xfce4-terminal -l 2>/dev/null)" ]; then
+    if [ "$theme" = dark ]; then
+        xfconf-query -c xfce4-terminal -p /color-foreground -r
+        xfconf-query -c xfce4-terminal -p /color-background -r
+        xfconf-query -c xfce4-terminal -p /color-palette -s "#000000;#cc0000;#4e9a06;#c4a000;#3465a4;#75507b;#06989a;#d3d7cf;#555753;#ef2929;#8ae234;#fce94f;#739fcf;#ad7fa8;#34e2e2;#eeeeec"
+    else
+        xfconf-query -c xfce4-terminal -p /color-foreground -s "#000000" -n -t string
+        xfconf-query -c xfce4-terminal -p /color-background -s "#ffffff" -n -t string
+        xfconf-query -c xfce4-terminal -p /color-palette -s "rgb(0,0,0);rgb(205,0,0);rgb(0,205,0);rgb(205,205,0);rgb(0,0,205);rgb(205,0,205);rgb(0,205,205);rgb(229,229,229);rgb(127,127,127);rgb(255,0,0);rgb(36,179,36);rgb(191,191,31);rgb(92,92,255);rgb(255,0,255);rgb(0,255,255);rgb(255,255,255)"
+    fi
+fi
+# }}}
+
+# xfce4-terminal (<1.1.0), using config file {{{
+# config path:
+# ~/.config/xfce4/terminal/terminalrc
 config_path=~/.config/xfce4/terminal/terminalrc
-old_config=$(grep -Ev '^Color' "$config_path")
-if [ -n "$old_config" ]; then
+old_config=$(grep -Ev '^Color' "$config_path" 2>/dev/null)
+if [ -e "$config_path" ] && [ -n "$old_config" ]; then
     {
         printf %s "$old_config"
         echo
