1015c88a1b46165fd175aa1be114aed495a8beb1
lxhillwind <lxhillwind@foxmail.com>
2023-11-18 14:55:47 +0800

    kitty fonts tweak (linux); add theme auto toggle for kitty
    remove unused config (nvim & alacritty)

--- .config/nvim/init.vim
+++
@@ -1,6 +0,0 @@
-for s:i in ['~/.vimrc', '~/_vimrc']
-    if filereadable(expand(s:i))
-        exe 'so' s:i
-        break
-    endif
-endfor
--- .local/share/dark-mode.d/to-dark.sh
+++ .local/share/dark-mode.d/to-dark.sh
@@ -20,6 +20,8 @@ case "${0##*/}" in
         exit 1 ;;
 esac
 
+# xfce & gtk theme {{{1
+
 # xfce-theme-manager: set --panels=0 to avoid reset other panels' size.
 #   (may not work; try to set in xfce-theme-manager -> Advanced -> "Panel Size")
 # xfce-theme-manager: set icons in a separate line, since it won't work if set with other options.
@@ -34,7 +36,7 @@ else
 fi
 xfce-theme-manager --icons=Papirus
 
-# qt theme on gtk desktop; application restart required! {{{
+# qt theme on gtk desktop; application restart required! {{{1
 if command -v kvantummanager >/dev/null; then
     if [ "$theme" = dark ]; then
         kvantummanager --set KvArcDark
@@ -42,17 +44,15 @@ if command -v kvantummanager >/dev/null; then
         kvantummanager --set KvArc
     fi
 fi
-# }}}
 
-# git-delta {{{
+# git-delta {{{1
 if [ "$theme" = dark ]; then
     git config --global delta.light false
 else
     git config --global delta.light true
 fi
-# }}}
 
-# xfce4-terminal (>=1.1.0), using xfconf {{{
+# xfce4-terminal (>=1.1.0), using xfconf {{{1
 #
 # How to get color config for light / dark theme?
 # Switch theme manually, and inspect config.
@@ -67,9 +67,8 @@ if [ -n "$(xfconf-query -c xfce4-terminal -l 2>/dev/null)" ]; then
         xfconf-query -c xfce4-terminal -p /color-palette -s "rgb(0,0,0);rgb(205,0,0);rgb(0,205,0);rgb(205,205,0);rgb(0,0,205);rgb(205,0,205);rgb(0,205,205);rgb(229,229,229);rgb(127,127,127);rgb(255,0,0);rgb(36,179,36);rgb(191,191,31);rgb(92,92,255);rgb(255,0,255);rgb(0,255,255);rgb(255,255,255)"
     fi
 fi
-# }}}
 
-# xfce4-terminal (<1.1.0), using config file {{{
+# xfce4-terminal (<1.1.0), using config file {{{1
 # config path:
 # ~/.config/xfce4/terminal/terminalrc
 config_path=~/.config/xfce4/terminal/terminalrc
@@ -87,8 +86,24 @@ if [ -e "$config_path" ] && [ -n "$old_config" ]; then
         fi
     } > "$config_path"
 fi
-# }}}
 
-# vim / gvim {{{
+# kitty terminal {{{1
+# requires files:
+# - ~/.config/kitty/Atom.conf
+# - ~/.config/kitty/AtomOneLight.conf
+# get them from:
+#   https://github.com/kovidgoyal/kitty-themes
+config_path=~/.config/kitty/local.conf
+sed -i '/include.*Atom.*conf/d' "$config_path"
+{
+    if [ "$theme" = dark ]; then
+        printf '%s\n%s\n' '#include ./AtomOneLight.conf' 'include ./Atom.conf'
+    else
+        printf '%s\n%s\n' 'include ./AtomOneLight.conf' '#include ./Atom.conf'
+    fi
+} >> "$config_path"
+
+pgrep -f kitty | xargs -r kill -SIGUSR1
+
+# vim / gvim {{{1
 pgrep -x 'vim|gvim' | xargs -r kill -SIGUSR1
-# }}}
